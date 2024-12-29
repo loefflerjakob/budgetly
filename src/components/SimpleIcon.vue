30c31de5ecce220ddf567c5b71993d02d6fd0837
loefflerjakob <jakob-loeffler@gmx.at>
2024-12-29 11:46:23 +0100

    new component: SimpleIcon

---
+++ src/components/SimpleIcon.vue
@@ -0,0 +1,33 @@
+<!--
+HOW TO ADD NEW ICONS TO THIS COMPONENT
+
+1. Go to https://tablericons.com/
+2. Set Stroke to 1.25 and Size to 24
+3. Search Icon, copy svg code
+4. Chooose a name for the icon, e.g. "icon-trash"
+5. Add icon to the "icon" constant in the iconSvg computed property
+-->
+
+<script lang="ts">
+export default {
+  name: 'SimpleIcon',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    iconSvg() {
+      const icons: Record<string, string> = {
+        'icon-trash': `<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" width=\"24\" height=\"24\" stroke-width=\"1.25\"><path d=\"M4 7l16 0\"></path><path d=\"M10 11l0 6\"></path><path d=\"M14 11l0 6\"></path><path d=\"M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12\"></path><path d=\"M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3\"></path></svg>`
+      };
+      return icons[this.name] || '';
+    },
+  },
+};
+</script>
+
+<template>
+  <span v-html="iconSvg"></span>
+</template>
--- src/components/TableRow.vue
+++ src/components/TableRow.vue
@@ -1,4 +1,6 @@
 <script lang="ts">
+import SimpleIcon from './SimpleIcon.vue';
+
 export default {
   name: 'TableRow',
   props: {
@@ -20,12 +22,15 @@ export default {
     },
     category: {
       type: String,
-      required: false, // Optional, daher bleibt die Prüfung bestehen, falls nicht übergeben
+      required: false,
     },
     rowIndex: {
       type: Number,
       required: true,
     }
+  },
+  components: {
+    SimpleIcon,
   },
   computed: {
     formattedDate() {
@@ -54,14 +59,7 @@ export default {
     <td class="p-3">{{ formattedDate }}</td>
     <td class="p-3">{{ category }}</td>
     <td class="p-3"><a>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-          stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="1.25">
-          <path d="M4 7l16 0"></path>
-          <path d="M10 11l0 6"></path>
-          <path d="M14 11l0 6"></path>
-          <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
-          <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
-        </svg>
+        <SimpleIcon name="icon-trash" />
       </a></td>
   </tr>
 </template>
