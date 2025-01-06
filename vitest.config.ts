ece3e7aeb92ea747607e7f41b3bd2a4a1ce48ccb
loefflerjakob <jakob-loeffler@gmx.at>
2025-01-06 10:00:19 +0100

    fix: update vitest.config.ts to resolve Vite config asynchronously

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,16 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-  }),
-)
+export default (async () => {
+  const resolvedViteConfig = await viteConfig
+  return mergeConfig(
+    resolvedViteConfig,
+    defineConfig({
+      test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url)),
+      },
+    }),
+  )
+})()
